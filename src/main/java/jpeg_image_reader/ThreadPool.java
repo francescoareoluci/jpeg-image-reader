f0960179d9d0622d8ebf36cec198e5c86a75d7a3
Francesco Areoluci <francesco.areoluci@magentalab.it>
2020-07-15 12:25:35 +0200

    Switched thread pool to singleton

--- src/main/java/jpeg_image_reader/ThreadPool.java
+++ src/main/java/jpeg_image_reader/ThreadPool.java
@@ -6,14 +6,31 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class ThreadPool {
 	static int DEFAULT_THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors();
 	
-	public ThreadPool() 
+	private ThreadPool() 
 	{
-		this.threadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE);
+		this.pool = (ThreadPoolExecutor) Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE);
+		this.pool.prestartAllCoreThreads();
 	}
+	
+	// static method to create instance of Singleton class 
+    public static ThreadPool getThreadPool() 
+   { 
+       // To ensure only one instance is created 
+    	if (instance == null) { 
+    		instance = new ThreadPool(); 
+      }
+    	
+        return instance; 
+    } 
 	
 	public void submitThread(Runnable r)
 	{
-		this.threadPool.submit(r);
+		this.pool.submit(r);
+	}
+	
+	public long getCompletedTask() 
+	{
+		return this.pool.getCompletedTaskCount();
 	}
 	
 	/**
@@ -29,8 +46,14 @@ public class ThreadPool {
 			return false;
 		}
 		
-		this.threadPool.setCorePoolSize(size);
+		this.pool.setCorePoolSize(size);
 		return true;
+	}
+	
+	public int getPoolSize()
+	{
+		System.out.println(this.pool.getPoolSize());
+		return this.pool.getPoolSize();
 	}
 	
 	/**
@@ -40,9 +63,10 @@ public class ThreadPool {
 	 */
 	public boolean closePool()
 	{
-		this.threadPool.shutdown();
+		this.pool.shutdown();
 		return true;
 	}
 	
-	private ThreadPoolExecutor threadPool;				///< Thread pool to handle parallel image loading
+	private ThreadPoolExecutor pool;				///< Thread pool to handle parallel image loading
+    private static ThreadPool instance = null; 
 }
